7466d95dd1c8575978f5fce6e529fdeff3738f81
Bascom3000 <112181817+Bascom3000@users.noreply.github.com>
2023-07-19 09:57:18 +0200

    delete .css

--- frontend/src/Components/HomeComponents/Home.jsx
+++ frontend/src/Components/HomeComponents/Home.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import "./Home.css";
 import HomeText from "./HomeText";
 
 function Home() {
--- frontend/src/Components/Login/Login.jsx
+++ frontend/src/Components/Login/Login.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useContext } from "react";
 import { Link } from "react-router-dom";
-import "./login.css";
 import { UserContext } from "../../Contexts/UserContext";
 
 function Login() {
