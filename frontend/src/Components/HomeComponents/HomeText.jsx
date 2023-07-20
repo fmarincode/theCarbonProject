e355769bcd38a6c657eb0a76452b9e603f7dbca5
Bascom3000 <112181817+Bascom3000@users.noreply.github.com>
2023-07-20 21:30:43 +0200

    test succesfull + edit homeText

--- frontend/src/Components/HomeComponents/HomeText.jsx
+++ frontend/src/Components/HomeComponents/HomeText.jsx
@@ -14,12 +14,12 @@ function HomeText() {
         <>
           <p className="text-[#242422] text-justify px-5 md:pr-10 md:pl-10 font-bold text-lg drop-shadow-lg">
             Bonjour, si tu es arrivé jusqu'ici c'est que notre ami Google fait
-            très bien son boulot. Ici, grâce à l'API de Carbon Interface, tu vas
-            pouvoir calculer le poids du rejet en CO2 de tes déplacements en
-            avion. Mais avant ça, un peu de latin : Provident culpa deserunt
-            quibusdam nostrum accusamus enim vitae. Repellat sapiente libero
-            necessitatibus non rem accusamus ad esse ullam maxime blanditiis
-            cupiditate ab harum praesentium!
+            très bien son boulot. Maintenant que tu es là, grâce à l'API de
+            Carbon Interface, tu vas pouvoir calculer le poids du rejet en CO2
+            de tes déplacements en avion. Mais avant ça, un peu de latin :
+            Provident culpa deserunt quibusdam nostrum accusamus enim vitae.
+            Repellat sapiente libero necessitatibus non rem accusamus ad esse
+            ullam maxime blanditiis cupiditate ab harum praesentium!
           </p>
           <div>
             <button
